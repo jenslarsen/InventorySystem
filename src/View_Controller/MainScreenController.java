9a48875c643013762271d46b8c76b01e45fc5e87
Jens Larsen <dasuperman@gmail.com>
2017-08-27 15:41:50 -0700

    Add skeleton controller

---
+++ src/View_Controller/MainScreenController.java
@@ -0,0 +1,105 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class MainScreenController {
+
+    @FXML
+    private Button partSearchButton;
+
+    @FXML
+    private TextField partSearchTextField;
+
+    @FXML
+    private TableColumn<?, ?> partPartIDCol;
+
+    @FXML
+    private TableColumn<?, ?> partPartNameCol;
+
+    @FXML
+    private TableColumn<?, ?> partInvLevCol;
+
+    @FXML
+    private TableColumn<?, ?> partPriceCol;
+
+    @FXML
+    private Button partAddButton;
+
+    @FXML
+    private Button partModifyButton;
+
+    @FXML
+    private Button partDeleteButton;
+
+    @FXML
+    private Button prodAddButton;
+
+    @FXML
+    private Button prodModifyButton;
+
+    @FXML
+    private Button prodDeleteButton;
+
+    @FXML
+    private TableColumn<?, ?> prodPartIDCol;
+
+    @FXML
+    private TableColumn<?, ?> prodPartNameCol;
+
+    @FXML
+    private TableColumn<?, ?> prodInvLevCol;
+
+    @FXML
+    private TableColumn<?, ?> prodPriceCol;
+
+    @FXML
+    private Button prodSearchButton;
+
+    @FXML
+    private TextField prodSearchTextField;
+
+    @FXML
+    void partAddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partDeleteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partModifyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partSearchButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prodAddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prodDeleteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prodModifyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prodSearchButtonClick(ActionEvent event) {
+
+    }
+
+}
