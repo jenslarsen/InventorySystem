8f871cdadf2f43a2ad07e9333114ee2aed2f3424
Jens Larsen <dasuperman@gmail.com>
2017-08-27 16:37:46 -0700

    Add skeleton controller

---
+++ src/View_Controller/PartScreenController.java
@@ -0,0 +1,72 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class PartScreenController {
+
+    @FXML
+    private RadioButton partOutsourcedRadio;
+
+    @FXML
+    private ToggleGroup inOutGroup;
+
+    @FXML
+    private RadioButton partInhouseRadio;
+
+    @FXML
+    private Label addModifyPartLabel;
+
+    @FXML
+    private TextField partIDTextField;
+
+    @FXML
+    private TextField partNameTextField;
+
+    @FXML
+    private TextField partInvTextField;
+
+    @FXML
+    private TextField partPriceTextField;
+
+    @FXML
+    private TextField partMaxTextField;
+
+    @FXML
+    private TextField partMachineTextField;
+
+    @FXML
+    private TextField partMinTextField;
+
+    @FXML
+    private Button partSaveButton;
+
+    @FXML
+    private Button partCancelButton;
+
+    @FXML
+    void partCancelButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partInhouseRadioClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partOutsourcedRadioClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void partSaveButtonClick(ActionEvent event) {
+
+    }
+
+}
