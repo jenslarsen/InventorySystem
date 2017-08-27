3d72f2d92a86501e42c946fdedf3a27e55574a19
Jens Larsen <dasuperman@gmail.com>
2017-08-26 17:50:44 -0700

    Add Inventory class

---
+++ src/Model/Inventory.java
@@ -0,0 +1,55 @@
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jens Larsen
+ */
+public class Inventory {
+    
+    private ArrayList<Product> products;
+    private ArrayList<Part> allParts;
+    
+    public void addProduct(Product productToAdd) {
+        this.products.add(productToAdd);
+    }
+    
+    public boolean removeProduct(Product productToRemove) {
+        if(products.contains(productToRemove)) {
+            products.remove(productToRemove);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public Product lookupProduct(int productID) {
+        return products.get(productID);
+    }
+    
+    public void updateProduct(int productID) {
+        // TODO I don't know what this method is supposed to do???
+    }
+    
+    public void addPart(Part partToAdd) {
+        this.allParts.add(partToAdd);
+    }
+    
+    public boolean deletePart(Part partToDelete) {
+        if(allParts.contains(partToDelete)) {
+            allParts.remove(partToDelete);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public Part lookupPart(int partID) {
+        return this.allParts.get(partID);
+    }
+    
+    public void updatePart(int partID) {
+        // TODO I don't know what this method is supposed to do???
+    }
+}
