4c28ab196ab53df9a6a7ce16f43f68d9aa31be21
Jens Larsen <dasuperman@gmail.com>
2017-08-31 21:16:17 -0700

    Go back to MainScreen when Cancel is pressed

--- src/View_Controller/ProductScreenController.java
+++ src/View_Controller/ProductScreenController.java
@@ -1,11 +1,16 @@
 package View_Controller;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ProductScreenController {
 
@@ -78,8 +83,17 @@ public class ProductScreenController {
     }
 
     @FXML
-    void prodCancelButtonClick(ActionEvent event) {
+    void prodCancelButtonClick(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
 
+        Scene scene = new Scene(root);
+        stage = (Stage)prodCancelButton.getScene().getWindow();
+
+
+        stage.setTitle("Inventory System");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
