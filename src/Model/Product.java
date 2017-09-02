14e393c1ac6e340184b22452e1b680b1ba168139
Jens Larsen <dasuperman@gmail.com>
2017-09-01 19:12:12 -0700

    Adds Constructors

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -1,15 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
 import javafx.collections.ObservableList;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  *
  * @author Jens Larsen
  */
 public class Product {
+
+    public Product(ObservableList<Part> associatedParts, int productID, String name, 
+            double price, int inStock, int min, int max) {
+        this.associatedParts = associatedParts;
+        this.productID = new SimpleIntegerProperty(productID);
+        this.name = new SimpleStringProperty(name);
+        this.price = new SimpleDoubleProperty(price);
+        this.inStock = new SimpleIntegerProperty(inStock);
+        this.min = new SimpleIntegerProperty(min);
+        this.max = new SimpleIntegerProperty(max);
+    }
 
     private ObservableList<Part> associatedParts;
     private IntegerProperty productID;
