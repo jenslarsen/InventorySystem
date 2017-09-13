9c9cd0e87ac61c1c6c91827f1a6ca52187bd41b4
Jens Larsen <dasuperman@gmail.com>
2017-09-12 19:42:03 -0700

    Moves everything back where it should be

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -17,10 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 
@@ -90,93 +87,10 @@ public class MainScreenController {
     private TextField prodSearchTextField;
 
     @FXML
-    private RadioButton partOutsourcedRadio;
-
-    @FXML
-    private ToggleGroup inOutGroup;
-
-    @FXML
-    private RadioButton partInhouseRadio;
-
-    @FXML
-    private Label addModifyPartLabel;
-
-    @FXML
-    private TextField partIDTextField;
-
-    @FXML
-    private TextField partNameTextField;
-
-    @FXML
-    private TextField partInvTextField;
-
-    @FXML
-    private TextField partPriceTextField;
-
-    @FXML
-    private TextField partMaxTextField;
-
-    @FXML
-    private TextField partMachineTextField;
-
-    @FXML
-    private TextField partMinTextField;
-
-    @FXML
-    private Button partSaveButton;
-
-    @FXML
-    private Button partCancelButton;
-
-    @FXML
-    private Label addPartMachLabel;
-
-    @FXML
-    boolean inHousePart;
-
-    @FXML
-    void partCancelButtonClick(ActionEvent event) throws IOException {
-        Stage stage = (Stage) partCancelButton.getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    void partInhouseRadioClick(ActionEvent event) {
-        addPartMachLabel.setText("Machine ID");
-        inHousePart = true;
-    }
-
-    @FXML
-    void partOutsourcedRadioClick(ActionEvent event) {
-        addPartMachLabel.setText("Company");
-        inHousePart = false;
-    }
-
-    @FXML
-    void partSaveButtonClick(ActionEvent event) throws Exception {
-
-        try {
-            int partID = Integer.parseInt(partIDTextField.getText());
-            String name = partNameTextField.getText();
-            int inv = Integer.parseInt(partInvTextField.getText());
-            double price = Double.parseDouble(partPriceTextField.getText());
-            int max = Integer.parseInt(partMaxTextField.getText());
-            int min = Integer.parseInt(partMinTextField.getText());
-
-            if (inHousePart) {
-                int machineID = Integer.parseInt(partMachineTextField.getText());
-            } else {
-                String company = partMachineTextField.getText();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input");
-        }
-    }
-
-    @FXML
     void partAddButtonClick(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+
         stage.setScene(new Scene(root));
         stage.setTitle("Add Part");
         stage.initModality(Modality.APPLICATION_MODAL);
@@ -185,6 +99,8 @@ public class MainScreenController {
         stage.setResizable(false);
 
         stage.showAndWait();
+        //partInhouseRadio.setSelected(true);
+
     }
 
     @FXML
@@ -247,8 +163,6 @@ public class MainScreenController {
 
     @FXML
     public void initialize() {
-        System.out.println("Initializing MainScreen");
-        partInhouseRadio.setSelected(true);
 
         parts = FXCollections.observableArrayList();
         products = FXCollections.observableArrayList();
