889fa00434154a4b0315ee33136c24e2a826e7c5
Jens Larsen <dasuperman@gmail.com>
2017-09-23 19:19:34 -0700

    Adds overloaded deletePart method

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -8,53 +8,58 @@ import javafx.collections.ObservableList;
  * @author Jens Larsen
  */
 public class Inventory {
-    
+
     public static ObservableList<Product> products = FXCollections.observableArrayList();
     public static ObservableList<Part> parts = FXCollections.observableArrayList();
 
     public static void addProduct(Product productToAdd) {
         Inventory.products.add(productToAdd);
     }
-    
+
     public static boolean removeProduct(Product productToRemove) {
-        if(products.contains(productToRemove)) {
+        if (products.contains(productToRemove)) {
             products.remove(productToRemove);
             return true;
         } else {
             return false;
         }
     }
-    
+
     public static Product lookupProduct(int productID) {
         return products.get(productID);
     }
-    
+
     public static void updateProduct(int productID) {
         // TODO I don't know what this method is supposed to do???
     }
-    
+
     public static void addPart(Part partToAdd) {
         Inventory.parts.add(partToAdd);
     }
-    
+
     public static boolean deletePart(Part partToDelete) {
-        if(parts.contains(partToDelete)) {
+        if (parts.contains(partToDelete)) {
             parts.remove(partToDelete);
             return true;
         } else {
             return false;
         }
     }
-    
+
     public static boolean deletePart(int index) {
-        parts.remove(index);
-        return true;
+
+        if (index < 0 || index > parts.size()) {
+            parts.remove(index);
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
     public static Part lookupPart(int partID) {
         return Inventory.parts.get(partID);
     }
-    
+
     public static void updatePart(int partID, Part partToUpdate) {
         Inventory.parts.set(partID, partToUpdate);
     }
